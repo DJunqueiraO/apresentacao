b88679546552ba8c6187f1816460e383e28c0af0
FOURSYS\djunqueira <Batata>
2023-10-03 16:36:14 -0300

    ajustes hobbies

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,10 @@ export function Home() {
     return(
         <div className='Home'>
             <h1>DANIEL JUNQUEIRA OLIVEIRA</h1>
-            <h2>26 anos, Pinheiral/RJ</h2>
+            <h2>
+                26 anos, Pinheiral/RJ<br/>
+                Guitarra/Violão🎸, Jogos🕹️, Programar🖥️
+            </h2>
             <p>
                 Desenvolvedor Foursys<br/>
             </p>
